f2da84b107af2a69e73997b8428a17990d2b6a47
Francois Chaumet <theleapingwhale@gmail.com>
2025-07-15 06:01:23 +0000

    fix: Rename fields, change data type and correct duplicates issue

--- models/staging/bikes_database/stg_bikes_database__customers.sql
+++ models/staging/bikes_database/stg_bikes_database__customers.sql
@@ -1,12 +1,12 @@
 -- TODO: anonymize instead of not importing?
 SELECT
-    customer_id,
-    first_name,
-    last_name,
---    phone,
---    email,
---    street,
-    city,
-    state,
-    zip_code
+    customer_id, -- 1445 distinct
+    first_name AS customer_first_name,
+    last_name AS customer_last_name,
+--    phone AS customer_phone,
+--    email AS customer_email,
+--    street AS customer_street,
+    city AS customer_city, -- 195 distinct
+    state AS customer_state, -- 3 distinct
+    zip_code AS customer_zip_code -- 195 distinct
 FROM {{ source('bike_raw_data', 'customers') }}
--- models/staging/bikes_database/stg_bikes_database__stocks.sql
+++ models/staging/bikes_database/stg_bikes_database__stocks.sql
@@ -1,6 +1,6 @@
 SELECT
-    CONCAT(store_id, '_', product_id) AS stock_id,
-    store_id,
-    product_id,
+    CONCAT(store_id, '_', product_id) AS stock_id, -- 3 x 313 = 939 distinct
+    store_id, -- 3 distinct
+    product_id, -- 313 distinct
     quantity
 FROM {{ source('bike_raw_data', 'stocks') }}
